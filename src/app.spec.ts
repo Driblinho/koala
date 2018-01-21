d1267f85380dd9f59c9351da8567f8c8cfa59b45
Ireneusz Kuliga <irekkuliga@gmail.com>
2018-01-21 20:18:43 +0100

    fix test

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,13 +1,10 @@
 import test from "ava";
 import * as request from "supertest";
-import koalaServer from "./app"
+import { koalaServer } from "./app";
 
-test.afterEach(() => {
-	koalaServer.close();
-});
+test.afterEach(() => { koalaServer.close(); });
 
 test("Serwer return 200 status", async t => {
-	const response = await request(koalaServer).get("/");
-	t.is(response.status, 200);
+    const response = await request(koalaServer).get("/");
+    t.is(response.status, 200);
 });
-
